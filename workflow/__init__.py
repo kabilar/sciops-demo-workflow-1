743266b9b12a82bb7f9e9a42f59670d5de0c3c13
Thinh Nguyen <thinh@vathes.com>
2021-09-21 17:07:22 -0500

    bugfix

--- workflow/__init__.py
+++ workflow/__init__.py
@@ -7,14 +7,14 @@ if 'custom' not in dj.config:
 
 # overwrite dj.config['custom'] values with environment variables if available
 
-dj.config['custom']['database.prefix'] = os.get(
+dj.config['custom']['database.prefix'] = os.getenv(
     'DATABASE_PREFIX',
     dj.config['custom'].get('database.prefix', ''))
 
-dj.config['custom']['ephys_root_data_dir'] = os.get(
+dj.config['custom']['ephys_root_data_dir'] = os.getenv(
     'EPHYS_ROOT_DATA_DIR',
     dj.config['custom'].get('ephys_root_data_dir', ''))
 
-dj.config['custom']['ephys_processed_data_dir'] = os.get(
+dj.config['custom']['ephys_processed_data_dir'] = os.getenv(
     'EPHYS_PROCESSED_DATA_DIR',
     dj.config['custom'].get('ephys_processed_data_dir', ''))
